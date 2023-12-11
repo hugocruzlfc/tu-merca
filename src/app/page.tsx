fa9c13bea8261f75daf4ebf6bfc86fedc885034f
hugocruzlfc <hugocruzlfc@gmail.com>
2023-12-11 10:41:21 +0100

    feat: adding register and auth api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
-export default function Home() {
+import {
+  LoginButton,
+  LogoutButton,
+  LoyaltyButton,
+  ProfileButton,
+  RegisterButton,
+} from "@/components/Buttons";
+import { authOptions } from "@/lib";
+import { getServerSession } from "next-auth";
+
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  console.log(session);
   return (
     <main className="flex  flex-col items-center p-24">
       <div className="flex flex-row items-center gap-4 mb-10">
         <p>Landing page ⚙️</p>
+        <div>
+          <LoginButton />
+          <RegisterButton />
+          <LogoutButton />
+          <ProfileButton />
+          <LoyaltyButton />
+        </div>
       </div>
     </main>
   );
