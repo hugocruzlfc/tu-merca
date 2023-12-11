f185df23113b9beebcd8a0eb92cfa520ffb2e77f
hugocruzlfc <hugocruzlfc@gmail.com>
2023-12-11 11:42:40 +0100

    feat: protected routes with middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,10 @@ import {
   ProfileButton,
   RegisterButton,
 } from "@/components/Buttons";
-import { authOptions } from "@/lib";
-import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-  console.log(session);
+  // redirect("/profile")
   return (
     <main className="flex  flex-col items-center p-24">
       <div className="flex flex-row items-center gap-4 mb-10">
