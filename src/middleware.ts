62fadeaa0dcb270300105e1aaa000f42fab14bac
hugocruzlfc <hugocruzlfc@gmail.com>
2023-12-12 17:05:49 +0100

    fix: update middleware matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,7 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/((?!$|register|api|login).*)"],
+  matcher: [
+    "/((?!$|register|api|login|_next/static|_next/image|assets|favicon.ico|logo.png|sw.js).*)",
+  ],
 };
